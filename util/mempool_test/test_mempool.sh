0a09a6143f7545db496d9df6aaec9c55a6ecde91
触礁 <zdy0_2004@qq.com>
2017-02-21 20:46:55 +0800

    add mempool && cthread_pool test

---
+++ util/mempool_test/test_mempool.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+rm *core*
+rm *.exe *.dat *.log *.out
+rm roll*
+rm *.o
+rm *2017*
+rm redo*
+clear
+g++ mempool_test.c -O3 -g -I ../../mempool_test.h  -lpthread -o mempool_test.exe 
+echo ""
+echo ""
+
+./mempool_test.exe
