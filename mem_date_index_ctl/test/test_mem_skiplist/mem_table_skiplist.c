218fe3fa30255580bf35d34a57824187b6c39cf7
触礁 <zdy0_2004@qq.com>
2017-04-28 01:30:28 +0800

    Update mem_table_skiplist.c

--- mem_date_index_ctl/test/test_mem_skiplist/mem_table_skiplist.c
+++ mem_date_index_ctl/test/test_mem_skiplist/mem_table_skiplist.c
@@ -1,56 +1,4 @@
 #include "../../mem_skiplist_index.h"
-#define START_TRANS_REDO_DATA_WRITER_ERR  90008
-
-// 线程参数
-typedef struct thread_param_test
-{
-	mem_skiplist_index_t *mem_skiplist_index  ;
-	mem_table_config_t * mem_config;
-// 测试插入数据
-struct record_t * record_ptr;
-long block_no;
-long long  trans_no;
-}thread_param_test;
-
-
-
-void * thread_fun(void * arg)
-{
-mem_skiplist_index_t *mem_skiplist_index   = ((thread_param_test*)arg)->mem_skiplist_index ;
-
-long k=0;
-//long end = 1000*500*2;
-//long end = 10000*2;
-long end = 10000*10;
-long array[end];
-
-for(;k<end;++k){
-   //插入索引
-mem_skiplist_entry_t index1;
-index1.lkey = random();
-index1.block_no =1;
-index1.record_num = 1;
-
-array[k] = index1.lkey;
-struct  record_t * last_insert_skiplist_entry;
-//TICK
-mem_skiplist_insert( mem_skiplist_index ,&index1,&last_insert_skiplist_entry );
-//TOCK	
-}
-
-DEBUG("START DELETE!\n");
-k=0;
-for(;k<end;++k){
-//删除索引
-mem_skiplist_entry_t index1;
-index1.lkey = array[k];
-index1.block_no =1;
-index1.record_num = 1;
-
-//mem_skiplist_delete( mem_skiplist_index ,&index1 );										
-}
-
-}
 
 
 int create_block(mem_block_t **mb,char * file_name)
@@ -141,7 +89,7 @@ if(0!=(err=mem_table_create(&mem_table,mem_config)))DEBUG("mem_table_create err 
 //建跳表索引配置
 mem_skiplist_index_config_t * skiplist_config		= NULL;
 //建skiplist配置信息
-if(0!=(err=create_mem_skiplist_config(&skiplist_config,2048*102400,"./first_index_name\0",16)))DEBUG("mem_table_create err is %d\n",err);
+if(0!=(err=create_mem_skiplist_config(&skiplist_config,20480,"./first_index_name\0",4)))DEBUG("mem_table_create err is %d\n",err);
 //建跳表索引
 mem_skiplist_index_t  			* mem_skiplist_index	= NULL;
 if(0!=(err=mem_skiplist_create(&mem_skiplist_index,mem_table,skiplist_config)))DEBUG("mem_table_create err is %d\n",err);
@@ -151,39 +99,109 @@ if(0!=(err=mem_skiplist_init( mem_skiplist_index)))DEBUG("mem_table_create err i
 DEBUG("mem_skiplist_index is %0x\n",mem_skiplist_index);
 
 
-//线程数
-int tnum = 5;
-pthread_t th[tnum];
+////// 测试插入数据
+////struct record_t * record_ptr =NULL;
+////long block_no;
+////char buf[]="abcd";
+////if(0!=(err=mem_table_insert_record(mem_table,&record_ptr,&block_no,buf)))DEBUG("mem_table_insert_record err is %d\n",err);
+////char buf1[]="xyzm";
+////if(0!=(err=mem_table_insert_record(mem_table,&record_ptr,&block_no,buf1)))DEBUG("mem_table_insert_record err is %d\n",err);
+//////if(0!=(err=mem_table_insert_record(mem_table,&record_ptr,&block_no,buf1)))DEBUG("mem_table_insert_record err is %d\n",err);
+////
+////// 测试修改数据
+////char buf2[]="opqw";
+////if(0!=(err=mem_table_update_record(mem_table,record_ptr,buf2)))DEBUG("mem_table_update_record err is %d\n",err);
+//////if(0!=(err=mem_table_force_update_record(mem_table,record_ptr,buf2)))DEBUG("mem_table_update_record err is %d\n",err);
+//////if(0!=(err=mem_table_safe_update_record(mem_table,record_ptr,buf2)))DEBUG("mem_table_update_record err is %d\n",err);
+////
+////// 测试读取数据
+////char buf3[256]={};
+////if(0!=(err=mem_table_read_record(mem_table,record_ptr,buf3)))DEBUG("mem_table_read_record err is %d\n",err);
+//////if(0!=(err=mem_table_force_read_record(mem_table,record_ptr,buf3)))DEBUG("mem_table_read_record err is %d\n",err);
+//////if(0!=(err=mem_table_safe_read_record(mem_table,record_ptr,buf3)))DEBUG("mem_table_read_record err is %d\n",err);
+////
+////// 测试删除数据
+//////if(0!=(err=mem_table_del_record(mem_table,record_ptr)))DEBUG("mem_table_del_record err is %d\n",err);
+//////if(0!=(err=mem_table_force_del_record(mem_table,record_ptr)))DEBUG("mem_table_del_record err is %d\n",err);
+////if(0!=(err=mem_table_safe_del_record(mem_table,record_ptr)))DEBUG("mem_table_del_record err is %d\n",err);
+	//  DEBUG("mem_skiplist_index_table's name is %s, addr is %0x .\n",mem_skiplist_index->heap_space->config.table_name,mem_skiplist_index->heap_space->config);
 
-thread_param_test tpt;
-tpt.mem_skiplist_index = mem_skiplist_index;
-
-	
-
-int i = 0;
-for(;i<tnum;++i){
-    int ret;  
-   
-    ret = pthread_create( &(th[i]), NULL, thread_fun, &tpt ); 
-	  if( ret != 0 ){  
-        ERROR( "Create log writer thread error!\n");  
-        return START_TRANS_REDO_DATA_WRITER_ERR;  
-    }  
-  }
-  TICK
-int j = 0;
-for(;j<tnum;++j){
-    int ret;  
-   
-    ret = pthread_join( (th[j]), NULL);  
-  }
 
 
+//插入索引
+mem_skiplist_entry_t index1;
+index1.lkey = 1;
+index1.block_no =1;
+index1.record_num = 1;
 
-	printf("main  used " );
-	TOCK
- // mem_skiplist_print_each_level(mem_skiplist_index);
+//插入索引
+mem_skiplist_entry_t index2;
+index2.lkey = 2;
+index2.block_no =4;
+index2.record_num = 4;
 
+//插入索引
+mem_skiplist_entry_t index3;
+index3.lkey = 3;
+index3.block_no =3;
+index3.record_num = 3;
+
+//插入索引
+mem_skiplist_entry_t index5;
+index5.lkey = 5;
+index5.block_no =5;
+index5.record_num = 5;
+
+//插入索引
+mem_skiplist_entry_t index6;
+index6.lkey = 6;
+index6.block_no =6;
+index6.record_num = 6;
+
+struct  record_t * last_insert_skiplist_entry;
+
+if(0!=(mem_skiplist_insert(mem_skiplist_index,&index1, &last_insert_skiplist_entry))){ERROR("mem_skiplist_insert err  is %d\n",err);return err;}
+
+
+//if(0!=(mem_skiplist_insert_help(mem_skiplist_index, NULL,3,&index1, &last_insert_skiplist_entry))){ERROR("mem_skiplist_insert err1 is %d\n",err);return err;}
+if(0!=(mem_skiplist_insert(mem_skiplist_index, &index2, &last_insert_skiplist_entry))){ERROR("mem_skiplist_insert err2 is %d\n",err);return err;}
+if(0!=(mem_skiplist_insert(mem_skiplist_index, &index3, &last_insert_skiplist_entry))){ERROR("mem_skiplist_insert err3 is %d\n",err);return err;}
+//if(0!=(mem_skiplist_insert(mem_skiplist_index, &index5, &last_insert_skiplist_entry))){ERROR("mem_skiplist_insert err4 is %d\n",err);return err;}
+//if(0!=(mem_skiplist_insert(mem_skiplist_index, &index6, &last_insert_skiplist_entry))){ERROR("mem_skiplist_insert err5 is %d\n",err);return err;}
+
+
+
+//if(0!=(err=mem_skiplist_delete(mem_skiplist_index, &index1))){ERROR("mem_skiplist_delete err7 is %d\n",err);return err;}
+
+
+//if(0!=(err=mem_skiplist_search(mem_skiplist_index, mem_skiplist_index->root,3,&index4))){ERROR("mem_skiplist_search err6 is %d\n",err);;return;}
+//if(0!=(err=mem_skiplist_delete_help(mem_skiplist_index, NULL,0, &index1))){ERROR("mem_skiplist_delete err7 is %d\n",err);return err;}
+//  //	if(0!=(err=mem_skiplist_delete(mem_skiplist_index, mem_skiplist_index->root,   index4)))DEBUG("mem_skiplist_delete err is %d\n",err);
+//
+////使用此函数查找 >= key 的第一个节点
+//if(0!=(err= mem_skiplist_upper_bound(
+//																	mem_skiplist_index,
+//																	mem_skiplist_index->root,
+//																	0,
+//																	&index11
+//																	)))DEBUG("mem_skiplist_upper_bound err is %d\n",err);;
+//
+//mem_skiplist_entry_t *  right = mem_skiplist_right(mem_skiplist_index,mem_skiplist_index->root);
+//mem_skiplist_entry_t *  left  = mem_skiplist_left(mem_skiplist_index,mem_skiplist_index->root);
+//mem_skiplist_entry_t *  left_left  = mem_skiplist_left(mem_skiplist_index,left);
+//mem_skiplist_entry_t *  left_left_left  = mem_skiplist_left(mem_skiplist_index,left_left);
+//
+//
+//DEBUG("(root->skiplist_lkey,root->block_no,root->record_num) = (%d,%d,%d,%d)\n",mem_skiplist_index->root->skiplist_lkey,mem_skiplist_index->root->block_no,mem_skiplist_index->root->record_num,mem_skiplist_index->root->color);
+//DEBUG("(left->skiplist_lkey,left->block_no,left->record_num) = (%d,%d,%d,%d)\n",left->skiplist_lkey,left->block_no,left->record_num,left->color);
+//DEBUG("(right->skiplist_lkey,right->block_no,right->record_num) = (%d,%d,%d,%d)\n",right->skiplist_lkey,right->block_no,right->record_num,right->color);
+//
+//DEBUG("(left_left->skiplist_lkey,left_left->block_no,left_left->record_num) = (%d,%d,%d,%d)\n",left_left->skiplist_lkey,left_left->block_no,left_left->record_num,left_left->color);
+//
+//DEBUG("( left_left_left->lkey,left_left_left->block_no,left_left_left->record_num) = (%d,%d,%d,%d)\n",left_left_left->skiplist_lkey,left_left_left->block_no,left_left_left->record_num,left_left_left->color);
+//
+//DEBUG("( index11->lkey,index11->block_no,index11->record_num) = (%d,%d,%d,%d)\n",index11->skiplist_lkey,index11->block_no,index11->record_num,left_left_left->color);
+//
 
 if(0!=(err=mem_skiplist_index_close(mem_skiplist_index)))DEBUG("mem_skiplist_index_close err is %d\n",err);
 
