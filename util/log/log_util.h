534bd5321ef00f266868cc99e66cffe85428648e
触礁 <zdy0_2004@qq.com>
2017-07-26 00:04:34 +0800

    fix c++11 log format

--- util/log/log_util.h
+++ util/log/log_util.h
@@ -4,25 +4,9 @@
 
 
 #include <stdio.h>
+
 #include <pthread.h>
-#include <string.h>
-
-#define TICK \
-   struct timeval __tv1;\
-   gettimeofday(&__tv1, NULL);
-   
-
-#define TOCK \
-  struct timeval __tv2;\
-   gettimeofday(&__tv2, NULL);\
-   if(__tv1.tv_sec == __tv2.tv_sec)printf("%ld us \n", __tv2.tv_usec-__tv1.tv_usec );\
-   	else\
-   printf(" %ld us \n",  __tv2.tv_usec + (__tv2.tv_sec - __tv1.tv_sec )*1000000 - __tv1.tv_usec   );
-
-
-
-
-//#define __DEBUG__  
+#define __DEBUG__  
 
 
 #ifdef __cplusplus
@@ -33,9 +17,6 @@ extern "C" {
 
 #include <sys/time.h>
 char now_buffer[50] = { 0 };
-
-
-
 
 char * GetTime()
 {
@@ -77,20 +58,18 @@ int GetTimeForNAME(char *buffer)
 
     return 0;
 }
-#define IMPORTANT_INFO(format,...) printf("%s["__FILE__",%s() Line:%05d,tid:%ld]:>>>IMPORTANT_INFO "format"",GetTime(),__FUNCTION__, __LINE__, pthread_self() ,##__VA_ARGS__)  
-#define ERROR(format,...) printf("%s["__FILE__",%s() Line:%05d ,tid:%ld]:>>>>Err "format"",GetTime(),__FUNCTION__, __LINE__,pthread_self() , ##__VA_ARGS__)  
+#define IMPORTANT_INFO(format,...) printf("%s["__FILE__",%s() Line:%05d,tid:%ld]:>>>IMPORTANT_INFO " format "",GetTime(),__FUNCTION__, __LINE__, pthread_self() ,##__VA_ARGS__)  
+#define ERROR(format,...) printf("%s["__FILE__",%s() Line:%05d ,tid:%ld]:>>>>Err " format "",GetTime(),__FUNCTION__, __LINE__,pthread_self() , ##__VA_ARGS__)  
 
 #ifdef __DEBUG__  
-#define DEBUG(format,...) printf("%s["__FILE__",%s() Line:%05d,tid:%ld]: "format"",GetTime(),__FUNCTION__, __LINE__,pthread_self(), ##__VA_ARGS__)  
-//#define IMPORTANT_INFO(format,...) printf("%s["__FILE__",%s() Line:%05d]:>>>>IMPORTANT_INFO  "format"",GetTime(),__FUNCTION__, __LINE__, ##__VA_ARGS__)  
+#define DEBUG(format,...) printf("%s["__FILE__",%s() Line:%05d,tid:%ld]: " format "",GetTime(),__FUNCTION__, __LINE__,pthread_self(), ##__VA_ARGS__)  
+//#define IMPORTANT_INFO(format,...) printf("%s["__FILE__",%s() Line:%05d]:>>>>IMPORTANT_INFO  " format "",GetTime(),__FUNCTION__, __LINE__, ##__VA_ARGS__)  
 //#define IMPORTANT_INFO(format,...) 
 #else  
 #define DEBUG(format,...)  
-//#define ERROR(format,...) printf("%s["__FILE__",%s() Line:%05d]:>>>>Err  "format"",GetTime(),__FUNCTION__, __LINE__, ##__VA_ARGS__)  
-//#define IMPORTANT_INFO(format,...) printf("%s["__FILE__",%s() Line:%05d]:>>>>IMPORTANT_INFO  "format"",GetTime(),__FUNCTION__, __LINE__, ##__VA_ARGS__)  
+//#define ERROR(format,...) printf("%s["__FILE__",%s() Line:%05d]:>>>>Err  " format "",GetTime(),__FUNCTION__, __LINE__, ##__VA_ARGS__)  
+//#define IMPORTANT_INFO(format,...) printf("%s["__FILE__",%s() Line:%05d]:>>>>IMPORTANT_INFO  " format "",GetTime(),__FUNCTION__, __LINE__, ##__VA_ARGS__)  
 //#define IMPORTANT_INFO(format,...) 
-
-
 
 
 #endif  
