b5137efa146092b87692cb05510de4b380fdc1dc
触礁 <zdy0_2004@qq.com>
2017-07-31 11:24:45 +0800

    Update test_table_skiplist.sh

--- mem_date_index_ctl/test/test_mem_skiplist/test_table_skiplist.sh
+++ mem_date_index_ctl/test/test_mem_skiplist/test_table_skiplist.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 rm *core*
 rm *.exe *.dat
-rm *2016*
+rm *201*
 clear
 gcc mem_table_skiplist.c -g   -I ../../  -lpthread -o mem_table_skiplist.exe 
 ./mem_table_skiplist.exe >log.log
 cat log.log
 ipcs
-hexdump -e '16/1 "%02X " " | "' -e '16/1 "%_p" "\n"' ./first_index_name_heap.dat 
+hexdump -e '16/1 "%02X " " | "' -e '16/1 "%_p" "\n"' ./first_index_name*.dat 
 #
 #gcc mem_table_read_test.c -g  -I ../../  -lpthread -o mem_table_read_test.exe 
 #./mem_table_read_test.exe
