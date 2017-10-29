d10a20cad5ec7e72bbee85a5d70358569f88e629
触礁 <zdy0_2004@qq.com>
2017-10-29 17:28:57 +0800

    Add then test!

--- operations/test/exec_node_test.cpp
+++ operations/test/exec_node_test.cpp
@@ -1,6 +1,5 @@
 //  g++ -w -std=c++11  execute_node.cpp  -o cc.exe  
 #include"../execute_node.hpp"
-#include "../../util/cthread_pool.h"
 
 int fun4(int a,int b ,int c )
 {
@@ -13,19 +12,48 @@ int func4(char a,char b ,char c, char d)
 {
 	
 	printf("%c,%c,%c,%c\n",a,b,c,d);
+	sleep(3);
 	return 0;
 }
+
+int func2(int a,int b)
+{
+	printf("%d,%d\n",a,b);
+	return 0;
+}
+
+class void_func_helper_t
+{
+typedef void * (*fun_type)(void *);
+std::function<void * (void *)> __process ;
+
+public:
+fun_type process;
+
+void * help_process (void *)
+{
+__process((void *)0);
+return (void *)(0);
+}
+
+void set_func(std::function<void * (void *)> & _process )
+{
+  __process = std::move( _process ) ;
+
+}
+
+};
 
 int main()
 {
 		// 初始化线程池
-	  CThread_pool_t  *pool = (CThread_pool_t *) malloc (sizeof (CThread_pool_t));  
-    thread_pool_init (pool,8,1024);/*线程池中最多三个活动线程*/
-	  ready_queue.init(2048);
+	  cpp_thread_namespace::cpp_thread_pool_t  pool ;  
+    pool.thread_pool_init (2,1024);/*线程池中最多三个活动线程*/ 
 	
 	//typedef  std::function< int(int,int,int,int )> int_fun;
 	typedef int(*int_fun)(int,int,int );
 	typedef int(*char_fun)(char,char,char,char );
+	typedef int(*int2_fun)(int,int );
 	
 
   // 首节执行器类型
@@ -57,9 +85,38 @@ int main()
 	enode2.set_exec(ewraper2);
 	//设置2节点前继的前继节点
 	enode2.set_input_node(enode1 );
-	
+	   
+	enode2.set_put_once(
+	[&](std::function<void * (void *)> &process, void *arg)->int{
+	return	pool.add_task(process,arg);
+	}
+	 );
+	 
+	 enode2.set_call_once(
+	[&](){
+	   //cpp_thread_namespace::thread_routine_once(&pool);
+	   pool.thread_routine_once();
+			 }
+	 );
+	// then 函数	
+	//std::function<void * (void *)> process=[&](void * a){
+	//			enode2.then(func2,7,8);
+	//		return (void *)0;
+	//	};
+	//pool.add_task(process,(void *)0);
+
+	enode2.then(func2,7,8);
 	//运行2节点
 	enode2.try_execute();
+	
+	//std::function<void * (void *)> process2=[&](void * a){
+	//		enode2.try_execute();
+	//		return (void *)0;
+	//	};
+	//pool.add_task(process2,(void *)0);
+	
+	/*销毁线程池 不要忘记*/  
+    pool.thread_pool_destroy ();
 	
 	return 0;
 }
