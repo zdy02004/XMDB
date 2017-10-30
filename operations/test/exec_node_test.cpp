1f920b79710d4ed87e150dada553e25e3ec96a6d
触礁 <zdy0_2004@qq.com>
2017-10-31 01:08:59 +0800

    fix server core bug!

--- operations/test/exec_node_test.cpp
+++ operations/test/exec_node_test.cpp
@@ -8,7 +8,7 @@ int func2(int a,int b)
 	return 0;
 }
 
-int fun4(int a,int b ,int c )
+int func3(int a,int b ,int c )
 {
 	
 	printf("%d,%d,%d\n",a,b,c);
@@ -54,6 +54,8 @@ void set_func(std::function<void * (void *)> & _process )
 
 };
 
+
+
 int main()
 {
 		// 初始化线程池
@@ -73,10 +75,11 @@ int main()
   // 首节点类型
   typedef exec_node_type<empty_node_type1,empty_node_type1,ew1_type> en_type1;
   // 首节点
-  en_type1 enode1(1);
+  //en_type1 enode1(1);
+  en_type1 enode1(func3,1,2,3);
   // 首节执行器
-	ew1_type ewraper1(fun4,1,2,3);
-	enode1.set_exec(ewraper1);
+//	ew1_type ewraper1(func3,1,2,3);
+//	enode1.set_exec(ewraper1);
 	//运行首节点
 	enode1.try_execute();
 	
@@ -87,9 +90,10 @@ int main()
 	// 2节点的空兄弟节点类型
 	typedef exec_node_type<int,int,ew2_type> empty_node_type2;
 	//2节点类型
-  typedef exec_node_type<en_type1,empty_node_type2,ew2_type> en2_type;
+  typedef exec_node_type<en_type1,empty_node_type2,ew2_type> en_type2;
   //2节点
-	en2_type enode2(2);
+	en_type2 enode2(2);
+	//en_type2 enode2(func4,'a','b','c','d');
 	//2节点执行器
 	ew2_type ewraper2(func4,'a','b','c','d');
 	enode2.set_exec(ewraper2);
@@ -102,33 +106,58 @@ int main()
 	}
 	 );
 	 
-	 enode2.set_call_once(
-	[&](){
-	   //cpp_thread_namespace::thread_routine_once(&pool);
-	   pool.thread_routine_once();
-			 }
-	 );
-	// then 函数	
-	//std::function<void * (void *)> process=[&](void * a){
-	//			enode2.then(func2,7,8);
-	//		return (void *)0;
-	//	};
-	//pool.add_task(process,(void *)0);
+//	 enode2.set_call_once(
+//	[&](){
+//	   //cpp_thread_namespace::thread_routine_once(&pool);
+//	   pool.thread_routine_once();
+//			 }
+//	 );
 
-	auto a = enode2.then(func2,7,8).then(func2,9,10);
-	//enode2.then(func0);
+	 enode2.then(func2,7,8)
+	 			 .then(func2,9,10)
+	 			 .then(func2,11,12)
+	       .then(func0);
 	//运行2节点
-	//a->try_execute();
 	enode2.try_execute();
 
-	//std::function<void * (void *)> process2=[&](void * a){
-	//		enode2.try_execute();
-	//		return (void *)0;
-	//	};
-	//pool.add_task(process2,(void *)0);
+// 3节点
+en_type2 enode3(func4,'e','f','g','h');
+enode3.set_input_node(enode1 );
+enode3.set_put_once(
+[&](std::function<void * (void *)> &process, void *arg)->int{
+return	pool.add_task(process,arg);
+}
+ );
+ 
+ enode3.set_call_once(
+[&](){
+   //cpp_thread_namespace::thread_routine_once(&pool);
+   pool.thread_routine_once();
+		 }
+ );
+
+ enode3.then(func2,17,18)
+ 			 .then(func2,19,20)
+ 			 .then(func2,21,22);
+//运行3节点
+enode3.try_execute();
+  
+  
+  
+//快速语法
+ auto b = make_exec_node(func2,111,222);
+ b.set_put_once(
+[&](std::function<void * (void *)> &process, void *arg)->int{
+return	pool.add_task(process,arg);
+}
+ );
+ //
+ b.then(func4,'f','f','f','f');
+ b.try_execute();
+  
   	
-  	sleep(2);
-  	DEBUG("Send stop command!\n");
+  	sleep(7);
+  	ERROR("Send stop command!\n");
 	/*销毁线程池 不要忘记*/  
     pool.thread_pool_destroy ();
 	
