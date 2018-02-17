60e28fbe055cd0ce46cf2c80a80a59876f6728b6
触礁 <zdy0_2004@qq.com>
2018-02-18 04:30:13 +0800

    Update comp.sh

--- SQL/sql_parser/comp.sh
+++ SQL/sql_parser/comp.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+#添加关键字时，需要同时确保这3个地方一致
+#parser.hh:195:     SKIPLIST = 433,
+#parser.tab.c:754:     SKIPLIST = 433,
+#parser.tab.h:217:     SKIPLIST = 433,
+
 rm parser.tab.h
 rm *core*
 rm parser.tab.c
