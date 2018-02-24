3fd94880156b05f03d38019ea7b3f77497519edb
触礁 <zdy0_2004@qq.com>
2018-02-25 03:08:43 +0800

    Update CreateIndexAnalyser.hpp

--- SQL/sql_parser/CreateIndexAnalyser.hpp
+++ SQL/sql_parser/CreateIndexAnalyser.hpp
@@ -43,11 +43,12 @@ int get_table_name()
 	if(root->HasMember("children") )root = &(((*root)["children"].GetArray())[0]);
 	 
 	 CPP_DEBUG <<"before get_table_name \n";
-   //rapidjson_log( root );
+   rapidjson_log( root );
    
 	 if( root->HasMember("RELATION_NAME") )
 		{
 			table_name = const_cast<char *>( (*root)["RELATION_NAME"]["str_value_"].GetString() );
+
 		}
 		else
 		{
@@ -59,7 +60,8 @@ return 0;
 
 int get_index_name()
 {
-   
+   	 CPP_DEBUG <<"before int get_index_name() \n";
+
 	if( root->HasMember("INDEX_NAME") )
 		{
 			index_name = const_cast<char *>( (*root)["INDEX_NAME"]["str_value_"].GetString() );
@@ -74,40 +76,59 @@ return 0;
 
 int get_elemets()
 {
-	rapidjson::Value    *elemets = NULL;
+	CPP_DEBUG<<" get_elemets \n";
+
+	rapidjson::Value    *field_list = NULL;
 	if( root->HasMember("COLUMN_LIST") )
 		{
-			elemets =  &(*root)["COLUMN_LIST"];
+			field_list =  &(*root)["COLUMN_LIST"];
 		}
 		else
 		{
-			CPP_ERROR<<"THERE IS NO INDEX FIELDS!\n";
+			CPP_ERROR<<"THERE IS NO TABLE FIELDS!\n";
 			return CREATE_INDEX_HAS_NO_TABLE_FIELDS;
 		}
-		
-	for( auto &v : elemets->GetArray() )	
+	
+	std::string _field_name;
+
+	if(field_list->IsArray())
+	{
+	for( auto &v : field_list->GetArray() )	
 		{
-		if( v["tag"].GetInt() != T_COLUMN_DEFINITION || !v.HasMember("children") ) 
+
+		if( !v.IsObject() )continue;
+		if( v["tag"].GetInt() != T_IDENT  ) 
 			{
 				CPP_ERROR<<" FIELD DEFINATION FAILED !\n";
 				return CREATE_INDEX_FIELD_DEFINATION_FAILED;
 			}
-		rapidjson::Value * children = &( v["children"] );
-			
-		std::string field_name;
-		int field_type;
-			for ( auto &v0 : children->GetArray() )	
-				{
-					if( v0.HasMember("tag") && v0["tag"].GetInt() == T_IDENT )
-						{
-							field_name = std::string( v0["str_value_"].GetString() );
-						}
-				}
-		elements.emplace_back( field_name );
+		else
+			{
+				_field_name = const_cast<char *>( (v)["str_value_"].GetString() );
+				CPP_DEBUG<<" field_name " << _field_name <<endl;
+			}
+		elements.emplace_back( _field_name );
 		}
+	}
+	else 
+		{
+		if( (*field_list)["tag"].GetInt() != T_COLUMN_DEFINITION  ) 
+			{
+				CPP_ERROR<<" FIELD DEFINATION FAILED !\n";
+				return CREATE_INDEX_FIELD_DEFINATION_FAILED;
+			}
+		else
+			{
+				_field_name = std::string( (*field_list)["str_value_"].GetString() );
+				CPP_DEBUG<<" field_name " << _field_name <<endl;
+			}
+		elements.emplace_back( _field_name );
+			}
+		
 		
 return 0;
 }
+
 
 // 获得索引类型
 // 暂时不支持约束
@@ -119,6 +140,8 @@ int get_constraint_type()
 // 获得索引类型
 int get_index_type()
 {
+	CPP_DEBUG <<"before get_index_type() \n";
+
 	if( root->HasMember("INDEX_TYPE") )
 		{
 			int tag =  (*root)["INDEX_TYPE"]["tag"].GetInt();
@@ -133,6 +156,8 @@ int get_index_type()
 
 int get_create_info()
 {
+	CPP_DEBUG <<"before get_create_info() \n";
+
 	rapidjson::Value    *elemets = NULL;
 	if( root->HasMember("OPT_OPITION") )
 		{
@@ -144,23 +169,42 @@ int get_create_info()
 			return CREATE_INDEX_HAS_NO_OPT_OPITION;
 		}
 		
-		for( auto &v : elemets->GetArray() )	
+		rapidjson_log( elemets );
+
+		if(elemets->IsArray() )for( auto &v : elemets->GetArray() )	
 		{
-		  if( v["tag"].GetInt() != T_TABLET_BLOCK_SIZE || !v.HasMember("TABLET_BLOCK_SIZE") ) 
+		  if( v["tag"].GetInt() == T_TABLET_BLOCK_SIZE && v.HasMember("TABLET_BLOCK_SIZE") ) 
 			{
 				table_block_size = atol( v["TABLET_BLOCK_SIZE"]["str_value_"].GetString() )*1024*1024; // 默认是兆
 			}
-			if( v["tag"].GetInt() != T_TABLET_BLOCK_SIZE || !v.HasMember("EXTERN_SIZE") ) 
+			if( v["tag"].GetInt() == T_EXTERN_SIZE && v.HasMember("EXTERN_SIZE") ) 
 			{
 				extern_size = atol( v["EXTERN_SIZE"]["str_value_"].GetString() )*1024*1024; // 默认是兆
 			}
-			if( v["tag"].GetInt() != T_SKIP_LEVEL || !v.HasMember("SKIP_LEVEL") ) 
+			if( v["tag"].GetInt() == T_SKIP_LEVEL && v.HasMember("SKIP_LEVEL") ) 
 			{
-				extern_size = atoi( v["EXTERN_SIZE"]["str_value_"].GetString() ); // 默认是兆
+				skip_level = atoi( v["SKIP_LEVEL"]["str_value_"].GetString() ); // 默认是兆
 			}
 			
 			
 		}
+		else
+			{
+				
+			if( (*elemets)["tag"].GetInt() == T_TABLET_BLOCK_SIZE && (*elemets).HasMember("TABLET_BLOCK_SIZE") ) 
+			{
+				table_block_size = atol( (*elemets)["TABLET_BLOCK_SIZE"]["str_value_"].GetString() )*1024*1024; // 默认是兆
+			}
+			if( (*elemets)["tag"].GetInt() == T_EXTERN_SIZE && (*elemets).HasMember("EXTERN_SIZE") ) 
+			{
+				extern_size = atol( (*elemets)["EXTERN_SIZE"]["str_value_"].GetString() )*1024*1024; // 默认是兆
+			}
+			if( (*elemets)["tag"].GetInt() == T_SKIP_LEVEL && (*elemets).HasMember("SKIP_LEVEL") ) 
+			{
+				skip_level = atoi( (*elemets)["SKIP_LEVEL"]["str_value_"].GetString() ); // 默认是兆
+			}
+				
+			}
 		
 		
 }
@@ -169,17 +213,17 @@ int get_all()
 {
 	int ret = 0;
 	ret =get_table_name() ;
-	if( !ret )return ret;
+	if( ret )return ret;
 	ret =get_index_name() ;
-	if( !ret )return ret;
+	if( ret )return ret;
 	ret =get_index_type() ;
-	if( !ret )return ret;
+	if( ret )return ret;
 	ret = get_constraint_type();
-	if( !ret )return ret;
+	if( ret )return ret;
 	ret =get_elemets() ;
-	if( !ret )return ret;
+	if( ret )return ret;
 	ret =get_create_info() ;
-	if( !ret )return ret;
+	if( ret )return ret;
 	return 0;
 
 }
