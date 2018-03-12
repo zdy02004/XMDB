46f35202afaa4d57c9d3ef593949722e73e28af7
触礁 <zdy0_2004@qq.com>
2018-03-12 17:56:43 +0800

    change table locker position

--- sql_executor/plan_create_hash_index_node.h
+++ sql_executor/plan_create_hash_index_node.h
@@ -1,39 +1,38 @@
 /*
-g++ -C -std=c++11 plan_create_hash_index_node.h -w
+g++ -C -std=c++11 plan_create_skiplist_index_node.h -w
 
 */
-#ifndef PLAN_CREATE_HASH_INDEX_NODE
-#define PLAN_CREATE_HASH_INDEX_NODE
+#ifndef PLAN_CREATE_SKIPLIST_INDEX_NODE
+#define PLAN_CREATE_SKIPLIST_INDEX_NODE
 
 #include "plan_node.h"
-#include "../mem_date_index_ctl/mem_hash_index.h"
+#include "../mem_date_index_ctl/mem_skiplist_index.h"
 #include "../SQL/sql_parser/CreateIndexAnalyser.hpp"
 #include "create_index_online.h"
 
-#define PLAN_TYPE_CREATE_HASH_INDEX 								600
-#define CREATE_HASH_INDEX_NO_FIELD 								  601
-#define CREATE_HASH_INDEX_WRONG_FIELD						    603
+#define PLAN_TYPE_CREATE_SKIPLIST_INDEX 								700
+#define CREATE_SKIPLIST_INDEX_NO_FIELD 								  701
+#define CREATE_SKIPLIST_INDEX_WRONG_FIELD						    703
 
 
-int create_mem_hash_config(		mem_hash_index_config_t** hash_config ,
-															unsigned  long long array_block_size  ,
-															unsigned  long long link_block_size ,
-															int                 div               ,
-															char *index_name										)
+int create_mem_skiplist_config(mem_skiplist_index_config_t** skiplist_config,
+															unsigned  long long heap_block_size,
+															char *index_name,
+															int skip_level)
 {
-	(*hash_config) =(mem_hash_index_config_t *)malloc(MEM_HASH_CONFIG_SIZE);
-	//(*hash_config)->index_no			 	 = 1;
-	(*hash_config)->owner_table_no 	 = 1;
-	strcpy((*hash_config)->index_name,index_name);
-	(*hash_config)->array_block_size = array_block_size;
-	(*hash_config)->link_block_size  = link_block_size ;
-	(*hash_config)->div              = div;
+	*skiplist_config =(mem_skiplist_index_config_t *)malloc(MEM_SKIPLIST_CONFIG_SIZE);
+	(*skiplist_config)->index_no			  = 1;
+	(*skiplist_config)->owner_table_no  = 1;
+	strcpy((*skiplist_config)->index_name,index_name);
+	(*skiplist_config)->heap_block_size = heap_block_size;
+	(*skiplist_config)->max_level = skip_level;
+	
 	return 0;
 }
 
 
 //key func 构造 结果集的 record_tuple
-struct create_hash_index_node:public plan_node
+struct create_skiplist_index_node:public plan_node
 {
 std::string table_name;                //表名
 std::string index_name;                //表名
@@ -42,10 +41,11 @@ size_t 	block_size;										 //一个块大小
 size_t 	extend_block_size;             //块的扩展大小
 std::string path;											 //路径
 struct mem_table_t*                    mem_table;   //表指针
-mem_hash_index_t	*                    mem_hash_index; //HASH 索引指针
+mem_skiplist_index_t	*                mem_skiplist_index; //skiplist 索引指针
 int index_type;												// index 类型
-int div;
-create_hash_index_node( 
+int skip_level;														//跳表层数,默认4层
+
+create_skiplist_index_node( 
 							std::string& _table_name,
 							std::string& _index_name,
 							std::vector<std::string>& _field_vector,
@@ -61,16 +61,40 @@ create_hash_index_node(
 		block_size(_block_size),
 		extend_block_size(_extend_block_size),
 		path("./"),
-		index_type(INDEX_TYPE_HASH),
+		index_type(INDEX_TYPE_SKIP),
 		mem_table(NULL),
-		mem_hash_index(NULL),
-		div(3)
+		mem_skiplist_index(NULL),skip_level(4)
 		
 {
-plan_type = PLAN_TYPE_CREATE_HASH_INDEX;	
+plan_type = PLAN_TYPE_CREATE_SKIPLIST_INDEX;	
 }
 
-create_hash_index_node( 
+create_skiplist_index_node( 
+							std::string& _table_name,
+							std::string& _index_name,
+							std::vector<std::string>& _field_vector,
+							size_t 	_block_size,
+							size_t 	_extend_block_size,
+							int _skip_level,
+							rapidjson::Value& _json,
+  						rapidjson::Document * _Doc
+  									
+):plan_node(_json,_Doc),	
+		table_name(_table_name),
+		index_name(_index_name),
+		field_vector(_field_vector),
+		block_size(_block_size),
+		extend_block_size(_extend_block_size),
+		path("./"),
+		index_type(INDEX_TYPE_SKIP),
+		mem_table(NULL),
+		mem_skiplist_index(NULL),skip_level(_skip_level)
+		
+{
+plan_type = PLAN_TYPE_CREATE_SKIPLIST_INDEX;	
+}
+
+create_skiplist_index_node( 
 							std::string& _table_name,
 							std::string& _index_name,
 							std::vector<std::string>& _field_vector,
@@ -84,18 +108,14 @@ create_hash_index_node(
 		block_size(0),
 		extend_block_size(0),
 		path("./"),
-		index_type(INDEX_TYPE_HASH),
+		index_type(INDEX_TYPE_SKIP),
 		mem_table(NULL),
-		mem_hash_index(NULL),
-		div(3)
+		mem_skiplist_index(NULL),skip_level(4)
 {
-plan_type = PLAN_TYPE_CREATE_HASH_INDEX;	
+plan_type = PLAN_TYPE_CREATE_SKIPLIST_INDEX;	
 }
 
-void set_div(int _div)
-{
-	if( _div!=0 )	div = _div;
-}
+
 
 void set_path( std::string _path )
 {
@@ -131,7 +151,7 @@ int get_table_ptr()
 //检查该表中是否存在某个字段,并在表的字段上标记索引id
 int check_field()
 {
-	if(!field_vector.empty() && mem_table != NULL && mem_hash_index != NULL ){
+	if(!field_vector.empty() && mem_table != NULL && mem_skiplist_index != NULL ){
 		CPP_DEBUG<<"mem_table_lock "<<" \n";
 		mem_table_lock( &( mem_table->table_locker ) );
 		for( auto &v : field_vector )
@@ -139,11 +159,10 @@ int check_field()
 			if( !has_field( mem_table , v ) ){
 					CPP_ERROR<<"The Field "<<v<<" Not Exists in Table "<<table_name<<" \n";
 					CPP_DEBUG<<"mem_table_unlock "<<" \n";		
- 		      mem_table_unlock( &( mem_table->table_locker ) );
-				  return CREATE_HASH_INDEX_WRONG_FIELD;
+ 	       	mem_table_unlock( &( mem_table->table_locker ) );
+				  return CREATE_SKIPLIST_INDEX_WRONG_FIELD;
 				}
 				else { // 添加索引标记
-
 					 for(int i = 0;i < mem_table->config.field_used_num; ++i  )
  						{
  								field_t& field = mem_table->config.fields_table[i];
@@ -153,25 +172,19 @@ int check_field()
  							  	{
  							  		if(field.relate_index[j]!=0 ){
  							  		field.index_type[j] = index_type;
- 							  		field.relate_index[j] = mem_hash_index->config.index_no;  
+ 							  		field.relate_index[j] = mem_skiplist_index->config.index_no;  
  							  	  }
  							    }
  							  }
- 			
- 				 
-
-
+ 						}
 				}
 		}
-					}
- 		CPP_DEBUG<<"mem_table_unlock "<<" \n";		
- 		mem_table_unlock( &( mem_table->table_locker ) );
+	CPP_DEBUG<<"mem_table_unlock "<<" \n";		
+ 	mem_table_unlock( &( mem_table->table_locker ) );
 	}
 	else {
-				if(!field_vector.empty() )CPP_ERROR<<"In Create Index Statment, Fields is Empty !\n";
-				else if(mem_table == NULL)CPP_ERROR<<"In Create Index, Table ptr is Empty !\n";
-				else if(mem_hash_index == NULL)CPP_ERROR<<"In Create Index, Index ptr is Empty !\n";
-				return CREATE_HASH_INDEX_NO_FIELD;
+				CPP_ERROR<<"Field Empty !\n";
+				return CREATE_SKIPLIST_INDEX_NO_FIELD;
 			}
 	return 0;
 }
@@ -184,40 +197,41 @@ if( err )return err;
 	
 if( path !="./")index_name = path + index_name;
 
-//建 hash 索引配置
-mem_hash_index_config_t * hash_config		= NULL;
-//建 hash 配置信息
-if(0!=(err=create_mem_hash_config(&hash_config, block_size ,extend_block_size , div , const_cast<char *>(index_name.c_str()) )))
+//建 skiplist 索引配置
+mem_skiplist_index_config_t * skiplist_config		= NULL;
+//建 skiplist 配置信息
+if(0!=(err=create_mem_skiplist_config(&skiplist_config, block_size, const_cast<char *>(index_name.c_str())  ,skip_level ) ) )
 {
-	ERROR("mem_hash_index_create err is %d\n",err);
+	ERROR("mem_skiplist_index_create err is %d\n",err);
 	return err;
 }
 	
-//建 hash 索引
-if(0!=(err=mem_hash_index_create(&mem_hash_index,mem_table,hash_config)))
+//建 skiplist 索引
+if(0!=(err=mem_skiplist_create(&mem_skiplist_index,mem_table,skiplist_config)))
 	{
-		ERROR("mem_hash_index_create err is %d\n",err);
+		ERROR("mem_skiplist_index_create err is %d\n",err);
 		return err;
 	}
 
 //检查失败，回滚元数据
 if( check_field() )
 {
-	if(0!=(err=mem_hash_index_close(mem_hash_index)))
+	if(0!=(err=mem_skiplist_index_close(mem_skiplist_index)))
 		{
-			DEBUG("mem_hash_index_close err is %d\n",err);
+			DEBUG("mem_skiplist_index_close err is %d\n",err);
 			return err;
 		}
   
 }	
+
 
 //  目前只支持单读建索引
 if( 0 < field_vector.size() ){
 	
 	for( auto &v : field_vector  )
 	{
-	 online_create_index<mem_hash_entry_t> online_create_indexer( mem_table , v , index_type );
-	 err = online_create_indexer.execute(  mem_hash_index );
+	 online_create_index<mem_skiplist_entry_t> online_create_indexer( mem_table , v , index_type );
+	 err = online_create_indexer.execute(  mem_skiplist_index );
 	}
 }
 else
