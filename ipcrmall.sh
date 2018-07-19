c4f64cba8a330e8d8c1da3030d0e0fccdb7e7a4a
触礁 <zdy0_2004@qq.com>
2018-07-19 22:15:14 +0800

    Update ipcrmall.sh

--- ipcrmall.sh
+++ ipcrmall.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 set -x       
-
-ipcs -m | awk '{if(NR>3)print $2}' |
-while read s
-do
-    ipcrm -m $s
-done
+ipcs | grep `whoami` | grep 0x0000 | awk '{if (NF > 5 ) print "ipcrm -m "$2}' | sh
