dd4b0ea05fbea07f26584f47f1eafeda4c07d698
触礁 <zdy0_2004@qq.com>
2019-02-08 23:51:50 +0800

    assign from_list

--- SQL/sql_parser/update_analyser.hpp
+++ SQL/sql_parser/update_analyser.hpp
@@ -14,11 +14,11 @@ g++ -C -w -std=c++11 update_analyser.hpp
 
 typedef struct set_value
 {
-	char ibuf[sizeof(int)];
-  	char lbuf[sizeof(long)];
-  	char fbuf[sizeof(float)];
-  	std::string const_value;
-  	std::string const_type;
+char ibuf[sizeof(int)];
+char lbuf[sizeof(long)];
+char fbuf[sizeof(float)];
+std::string const_value;
+std::string const_type;
 }set_value;
 
 class UpdateAnalyser:public QueryAnalyser
@@ -26,26 +26,25 @@ class UpdateAnalyser:public QueryAnalyser
 public:
 
 rapidjson::Value  * set_lists;	
-rapidjson::Value  * table_name_v;		
 std::string table_name;	
 	
 std::map< std::string , set_value > set_map;
 	
-UpdateAnalyser(rapidjson::Value & value ,rapidjson::Document & doc_  ):QueryAnalyser( value , doc_ ),set_lists(NULL),table_name_v(NULL)
+UpdateAnalyser(rapidjson::Value & value ,rapidjson::Document & doc_  ):QueryAnalyser( value , doc_ ),set_lists(NULL)
 {
 	oper_type = BASIC_OPS_UPDATE ;
 	 //检查是否有 set_list                                        
 	check_and_assign_member(where_list,"SET_CLAUSE");   
-	check_and_assign_member(table_name_v,"TABLE_NAME");
+	check_and_assign_member(from_list,"TABLE_NAME");
 	
-	if( (*table_name_v).HasMember("TABLE_NAME") && (*table_name_v)["TABLE_NAME"].HasMember("str_value_") )
-	{
-		table_name=string((*table_name_v)["TABLE_NAME"]["str_value_"].GetString()); // 表名   
-		// 插入解析后的表
-		DEBUG(" tables.emplace_back( TableItem(%s,%s,%s) );\n", table_name.c_str(),table_name.c_str(),table_name.c_str() );
-  		tables.emplace_back( TableItem(table_name,table_name,table_name) );
-		resolve_update_list(set_lists);
-	}
+	if( (*from_list).HasMember("TABLE_NAME") && (*from_list)["TABLE_NAME"].HasMember("str_value_") )
+		{
+			  table_name=string((*from_list)["TABLE_NAME"]["str_value_"].GetString()); 	// 表名   
+			  // 插入解析后的表
+			 DEBUG(" tables.emplace_back( TableItem(%s,%s,%s) );\n", table_name.c_str(),table_name.c_str(),table_name.c_str() );
+  			 tables.emplace_back( TableItem(table_name,table_name,table_name) );
+			 resolve_update_list(set_lists);
+		}
 }
 
 void resolve_update_list_help( rapidjson::Value  * v,int level = 0 ){
@@ -56,17 +55,17 @@ DEBUG("resolve_update_list_help -------- { \n");
 				rapidjson_log(v);
   				if( (*v).HasMember("COLUNM_NAME") && (*v)["COLUNM_NAME"].HasMember("str_value_")    )  
   				{
-  					column_name=string( (*table_name_v)["COLUNM_NAME"]["str_value_"].GetString() ); 	// 表名   
+  					column_name=string( (*from_list)["COLUNM_NAME"]["str_value_"].GetString() ); 	// 表名   
   				}
   				
 					std::string const_value;
 					std::string const_type;
 					if( (*v).HasMember("EXPR") && (*v)["EXPR"].HasMember("str_value_")    )  
   				{
-  					const_value = string((*table_name_v)["const_type"]["str_value_"].GetString()); 	// 值
+  					const_value = string((*from_list)["const_type"]["str_value_"].GetString()); 	// 值
   					if( (*v)["EXPR"].HasMember("const_type")    )  
   					{
-  						const_type = string((*table_name_v)["const_type"]["str_value_"].GetString()); 	// 值类型   	
+  						const_type = string((*from_list)["const_type"]["str_value_"].GetString()); 	// 值类型   	
   					}  				 
   				}  		
   				set_value sv;
@@ -74,16 +73,16 @@ DEBUG("resolve_update_list_help -------- { \n");
   				  				
   				//转换为整型
   				if( const_type== "INTNUM"  )
-  				{
-  					long lval = std::stol(const_value);
-  					int  ival ;
-  					if( lval>-65535 && lval<=65535 )
   					{
-  						ival =  std::stoi(const_value);
-  						strncpy( const_cast<char *>( sv.ibuf ), (char *)(&ival) , sizeof(int)  );
+  						long lval = std::stol(const_value);
+  						int  ival ;
+  						if( lval>-65535 && lval<=65535 )
+  						{
+  							ival =  std::stoi(const_value);
+  							strncpy( const_cast<char *>( sv.ibuf ), (char *)(&ival) , sizeof(int)  );
+  						}
+  						strncpy( const_cast<char *>( sv.lbuf ) ,  (char *)(&lval) , sizeof(long)  );
   					}
-  					strncpy( const_cast<char *>( sv.lbuf ) ,  (char *)(&lval) , sizeof(long)  );
-  				}
   				//转换为浮点型	
   				else if( const_type == "APPROXNUM" )
   				{
@@ -96,13 +95,11 @@ DEBUG("resolve_update_list_help -------- { \n");
   				{
   					sv.const_value = const_value;
   				}
-  					
+ 					
  //放入列表  					
  set_map[column_name] = sv;
  DEBUG("resolve_update_list_help -------- } \n");
-
 }
-
 
 //解析 update_list
 void resolve_update_list( rapidjson::Value  * updatelist ,int level = 0 ){
