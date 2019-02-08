29dda6ef057c0409229f0131ab503e2a5ab6ffe3
触礁 <zdy0_2004@qq.com>
2019-02-08 23:27:37 +0800

    Update update_analyser.hpp

--- SQL/sql_parser/update_analyser.hpp
+++ SQL/sql_parser/update_analyser.hpp
@@ -14,11 +14,11 @@ g++ -C -w -std=c++11 update_analyser.hpp
 
 typedef struct set_value
 {
-		char ibuf[sizeof(int)];
+	char ibuf[sizeof(int)];
   	char lbuf[sizeof(long)];
   	char fbuf[sizeof(float)];
   	std::string const_value;
-	  std::string const_type;
+  	std::string const_type;
 }set_value;
 
 class UpdateAnalyser:public QueryAnalyser
@@ -34,76 +34,79 @@ std::map< std::string , set_value > set_map;
 UpdateAnalyser(rapidjson::Value & value ,rapidjson::Document & doc_  ):QueryAnalyser( value , doc_ ),set_lists(NULL),table_name_v(NULL)
 {
 	oper_type = BASIC_OPS_UPDATE ;
-	 //ºÏ≤È «∑Ò”– set_list                                        
+	 //Ê£ÄÊü•ÊòØÂê¶Êúâ set_list                                        
 	check_and_assign_member(where_list,"SET_CLAUSE");   
 	check_and_assign_member(table_name_v,"TABLE_NAME");
 	
 	if( (*table_name_v).HasMember("TABLE_NAME") && (*table_name_v)["TABLE_NAME"].HasMember("str_value_") )
-		{
-			 table_name=string((*table_name_v)["TABLE_NAME"]["str_value_"].GetString()); 	// ±Ì√˚   
-			 resolve_update_list(set_lists);
-		}
+	{
+		table_name=string((*table_name_v)["TABLE_NAME"]["str_value_"].GetString()); // Ë°®Âêç   
+		// ÊèíÂÖ•Ëß£ÊûêÂêéÁöÑË°®
+		DEBUG(" tables.emplace_back( TableItem(%s,%s,%s) );\n", table_name.c_str(),table_name.c_str(),table_name.c_str() );
+  		tables.emplace_back( TableItem(table_name,table_name,table_name) );
+		resolve_update_list(set_lists);
+	}
 }
 
 void resolve_update_list_help( rapidjson::Value  * v,int level = 0 ){
 
 DEBUG("resolve_update_list_help -------- { \n");
 					
-					std::string column_name;
-					rapidjson_log(v);
+				std::string column_name;
+				rapidjson_log(v);
   				if( (*v).HasMember("COLUNM_NAME") && (*v)["COLUNM_NAME"].HasMember("str_value_")    )  
   				{
-  					column_name=string( (*table_name_v)["COLUNM_NAME"]["str_value_"].GetString() ); 	// ±Ì√˚   
+  					column_name=string( (*table_name_v)["COLUNM_NAME"]["str_value_"].GetString() ); 	// Ë°®Âêç   
   				}
   				
 					std::string const_value;
 					std::string const_type;
 					if( (*v).HasMember("EXPR") && (*v)["EXPR"].HasMember("str_value_")    )  
   				{
-  					const_value = string((*table_name_v)["const_type"]["str_value_"].GetString()); 	// ÷µ
+  					const_value = string((*table_name_v)["const_type"]["str_value_"].GetString()); 	// ÂÄº
   					if( (*v)["EXPR"].HasMember("const_type")    )  
   					{
-  						const_type = string((*table_name_v)["const_type"]["str_value_"].GetString()); 	// ÷µ¿‡–Õ   	
+  						const_type = string((*table_name_v)["const_type"]["str_value_"].GetString()); 	// ÂÄºÁ±ªÂûã   	
   					}  				 
   				}  		
   				set_value sv;
   				sv.const_type  = const_type;
   				  				
-  				//◊™ªªŒ™’˚–Õ
+  				//ËΩ¨Êç¢‰∏∫Êï¥Âûã
   				if( const_type== "INTNUM"  )
+  				{
+  					long lval = std::stol(const_value);
+  					int  ival ;
+  					if( lval>-65535 && lval<=65535 )
   					{
-  						long lval = std::stol(const_value);
-  						int  ival ;
-  						if( lval>-65535 && lval<=65535 )
-  							{
-  								ival =  std::stoi(const_value);
-  								strncpy( const_cast<char *>( sv.ibuf ), (char *)(&ival) , sizeof(int)  );
-  							}
-  						strncpy( const_cast<char *>( sv.lbuf ) ,  (char *)(&lval) , sizeof(long)  );
+  						ival =  std::stoi(const_value);
+  						strncpy( const_cast<char *>( sv.ibuf ), (char *)(&ival) , sizeof(int)  );
   					}
-  				//◊™ªªŒ™∏°µ„–Õ	
+  					strncpy( const_cast<char *>( sv.lbuf ) ,  (char *)(&lval) , sizeof(long)  );
+  				}
+  				//ËΩ¨Êç¢‰∏∫ÊµÆÁÇπÂûã	
   				else if( const_type == "APPROXNUM" )
   				{
   					float fval = std::stof(const_value);
   					strncpy( const_cast<char *>( sv.fbuf ) , (char *)(&fval ), sizeof(float)  );
   				}
   				
-  				//◊™ªªŒ™◊÷∑˚¥Æ
+  				//ËΩ¨Êç¢‰∏∫Â≠óÁ¨¶‰∏≤
   				else if( const_type== "STRING" )
   				{
   					sv.const_value = const_value;
   				}
   					
- //∑≈»Î¡–±Ì  					
+ //ÊîæÂÖ•ÂàóË°®  					
  set_map[column_name] = sv;
  DEBUG("resolve_update_list_help -------- } \n");
 
 }
 
 
-//Ω‚Œˆ update_list
+//Ëß£Êûê update_list
 void resolve_update_list( rapidjson::Value  * updatelist ,int level = 0 ){
-   //±È¿˙ updatelist Ω´ ˝æ›±ÌÃÓ»Î table_names, Ω´◊”≤È—Ø ÃÓ»Î sub_querys
+   //ÈÅçÂéÜ updatelist Â∞ÜÊï∞ÊçÆË°®Â°´ÂÖ• table_names, Â∞ÜÂ≠êÊü•ËØ¢ Â°´ÂÖ• sub_querys
   	if( updatelist->IsArray() ) {
   		for (auto& v : ( (*updatelist).GetArray() )  ){  				
   				resolve_update_list( &(v),level);
